6be4f331da0efc6a378dd288d4be1eddf76a9e95
TeodorioNeto <83874983+TeodorioNeto@users.noreply.github.com>
2023-03-05 16:30:24 -0400

    RetirarPrimeiroElemento

--- FilaPrioridade.py
+++ FilaPrioridade.py
@@ -1,23 +1,29 @@
 class FilaDePrioridade:
     def __init__(self):
-        self.fila=[]
-    def inserir(self,prioridade,elemento):
+        self.fila = []
+
+    def inserir(self, elemento):
         if len(self.fila) == 0:
-            self.fila.append(prioridade,elemento)
+            self.fila.append(elemento)
         else:
-            self.fila.append(prioridade,elemento)
+            self.fila.append(elemento)
             for i in range((len(self.fila) // 2) - 1, -1, -1):
                 self.min_heapify(self.fila, len(self.fila), i)
-    
-    def min_heapify(vetor,tam,i):
-        menor=i
-        filhoEsq=(2*i)+1
-        filhoDir=(2*i)+2
-        if filhoEsq < tam and vetor[filhoEsq]<vetor[menor]:
+
+    def min_heapify(self, vetor, tam, i):
+        menor = i
+        filhoEsq = (2*i)+1
+        filhoDir = (2*i)+2
+        if filhoEsq < tam and vetor[filhoEsq] < vetor[menor]:
             menor = filhoEsq
-        if filhoDir < tam and vetor[filhoDir]<vetor[menor]:
+        if filhoDir < tam and vetor[filhoDir] < vetor[menor]:
             menor = filhoDir
-        if menor!=i:
+        if menor != i:
             vetor[i], vetor[menor] = vetor[menor], vetor[i]
-            self.min_heapify(vetor,tam,menor)
+            self.min_heapify(vetor, tam, menor)
+
+    def retiraPrimeiro(self):
+        primeiro=self.fila[0]
+        self.fila.remove(self.fila[0])
+        return primeiro
 
