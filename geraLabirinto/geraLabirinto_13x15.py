f35ba6d03ce4b141950ee27d017b99b338001379
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-03-05 22:25:25 -0400

    Correção

--- geraLabirinto/geraLabirinto_13x15.py
+++ geraLabirinto/geraLabirinto_13x15.py
@@ -14,17 +14,20 @@ class GeraLabirinto:
                     self.labirinto[i][j] = 2
 
     def cava_na_posicao(self, i, j):
+        guarda_pos_saida = False
         final_alcancado = False
         if(self.labirinto[i][j] == 2):                                                  #chegou na borda (possui saida)
             self.labirinto[i][j] = 1
             final_alcancado = True
-            return final_alcancado
+            guarda_pos_saida = (i,j)
+            return final_alcancado, guarda_pos_saida
         self.labirinto[i][j] = 1
-        return final_alcancado
+        return final_alcancado, guarda_pos_saida
     
     def cava_primeiro_campo(self, i, j, lista_coords):
         lista_coords.append((i,j))                                                      #armazenar a posicao inicial para posiocionar o jogador
         self.labirinto[i][j] = 1
+        return lista_coords[0]
     
     def cava_na_posicao_apos_encontrar_saida(self, i, j):                               #funcao utilizada apos a saida ser feita
         final_alcancado = False
@@ -34,7 +37,7 @@ class GeraLabirinto:
             self.labirinto[i][j] = 1
 
     def campos_iniciais(self, i_random, j_random, lista_coords):
-        self.cava_primeiro_campo(i_random, j_random, lista_coords)                      #gera o primeiro ponto
+        pos_jogador = self.cava_primeiro_campo(i_random, j_random, lista_coords)        #gera o primeiro ponto
         lista_coords.remove(lista_coords[0])
         lista_coords, local_aleatorio_escolhido, area_escavada = self.insere_e_verifica_coords_na_lista(self, i_random, j_random, True)
         i_random = local_aleatorio_escolhido[0]                                         #insere os quatro primeiros corredores em lista_coords (parte inicial)
@@ -43,22 +46,23 @@ class GeraLabirinto:
         i_random = local_aleatorio_escolhido[2]
         j_random = local_aleatorio_escolhido[3]
         self.cava_na_posicao(i_random, j_random)                                        #insere os quatro primeiros corredores em lista_coords (parte final)
+        return pos_jogador
 
     def escavacao_loop(self, i_random, j_random, lista_coords):
         lista_coords, local_aleatorio_escolhido, area_escavada = self.insere_e_verifica_coords_na_lista(self, i_random, j_random, True)
         while(lista_coords != []):
             lista_coords.remove(lista_coords[area_escavada])
             lista_coords, local_aleatorio_escolhido, area_escavada = self.insere_e_verifica_coords_na_lista(self, i_random, j_random, True)
-            i_random = local_aleatorio_escolhido[0]                                    #insere os proximos corredores potenciais em lista_coords (parte inicial)
+            i_random = local_aleatorio_escolhido[0]                                     #insere os proximos corredores potenciais em lista_coords (parte inicial)
             j_random = local_aleatorio_escolhido[1]
-            possui_saida = self.cava_na_posicao(i_random, j_random)
+            possui_saida, pos_saida = self.cava_na_posicao(i_random, j_random)
             if(possui_saida == True):                                                   #saida encontrada = sai do loop
-                break
+                return pos_saida
             i_random = local_aleatorio_escolhido[2]
             j_random = local_aleatorio_escolhido[3]
-            possui_saida = self.cava_na_posicao(i_random, j_random)                     #insere os proximos corredores potenciais em lista_coords (parte final)
+            possui_saida, pos_saida = self.cava_na_posicao(i_random, j_random)          #insere os proximos corredores potenciais em lista_coords (parte final)
             if(possui_saida == True):                                                   #saida encontrada = sai do loop
-                break
+                return pos_saida
 
         while(lista_coords != []):                                                      #apos encontrar a saida, as posicoes validas restantes sao cavadas ate o conteudo da lista esvaziar
             i_random = lista_coords[0][0]                                               #cava os proximos corredores restantes (parte inicial)
@@ -104,3 +108,6 @@ class GeraLabirinto:
             for j in range(colunas):
                 print("%s " % self.labirinto[i][j], end = " ")
             print()
+        
+    def pega_matriz(self):
+        return self.labirinto
--- geraLabirinto/main_geraLabirinto.py
+++ geraLabirinto/main_geraLabirinto.py
@@ -2,6 +2,10 @@ import geraLabirinto_13x15 as labirinto
 
 if __name__ == "__main__":
     matriz = labirinto.GeraLabirinto()
-    matriz.campos_iniciais(matriz.i_random, matriz.j_random, matriz.lista_coords)   #insere o primeiro ponto ma matriz #e insere os quatro primeiros corredores na lista
-    matriz.escavacao_loop(matriz.i_random, matriz.j_random, matriz.lista_coords)    #enquanto lista nao estiver vazia fica em loop
-    matriz.imprime_matriz(matriz.linhas, matriz.colunas)                            #matriz de um labirinto gerado aleatoriamente com uma unica saida
+    pos_jogador = matriz.campos_iniciais(matriz.i_random, matriz.j_random, matriz.lista_coords)     #insere o primeiro ponto ma matriz #e insere os quatro primeiros corredores na lista
+    pos_saida = matriz.escavacao_loop(matriz.i_random, matriz.j_random, matriz.lista_coords)        #enquanto lista nao estiver vazia fica em loop
+    matriz.imprime_matriz(matriz.linhas, matriz.colunas)                                            #matriz de um labirinto gerado aleatoriamente com uma unica saida
+    armazena_matriz = matriz.pega_matriz()
+    print("coords do primeiro campo cavado: ", pos_jogador)                                         #armazena a posicao inicial como sendo a posicao que o jogador vai comecar (auxilia no codigo da interface)
+    print("coords da saida: ", pos_saida)                                                           #armazena a posicao da saida (auxilia no codigo da interface)
+    print("conteudo matriz armazenada: ", armazena_matriz)                                          #armazena a matriz que vai ser passada para o codigo da interface, para entao gerar o visual do labirinto
