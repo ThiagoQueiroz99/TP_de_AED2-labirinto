8edf36c647976898a9adc322fcc7412362cf98ce
Alexandre da Fonseca Vieira <107149857+SnipAlex@users.noreply.github.com>
2023-03-05 22:17:04 -0400

    Revert "Codigo do jogo"
    This reverts commit 550e64fc72ef1cd38adb006e4d16d498fffe63ad.

--- main_geraLabirinto.py
+++
@@ -1,7 +0,0 @@
-import geraLabirinto_13x15 as labirinto
-
-if __name__ == "__main__":
-    matriz = labirinto.GeraLabirinto()
-    matriz.campos_iniciais(matriz.i_random, matriz.j_random, matriz.lista_coords)   #insere o primeiro ponto ma matriz #e insere os quatro primeiros corredores na lista
-    matriz.escavacao_loop(matriz.i_random, matriz.j_random, matriz.lista_coords)    #enquanto lista nao estiver vazia fica em loop
-    matriz.imprime_matriz(matriz.linhas, matriz.colunas)                            #matriz de um labirinto gerado aleatoriamente com uma unica saida
